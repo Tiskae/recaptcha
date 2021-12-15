bf36d2ed312858d90898f5183c05bd999c41eb66
user <user@users-MacBook-Pro.local>
2021-12-15 08:57:52 +0100

    handled error cases

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const form = document.querySelector("#form")
 
 form.addEventListener("submit", (event)=>{
     event.preventDefault()
-    alert("submitted successfully!")
+    // alert("submitted successfully!")
 })
